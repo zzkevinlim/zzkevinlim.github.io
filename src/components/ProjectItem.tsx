95aafc6bac66168e112c966848b18da8426cbb12
Kevin Lim <zzkevinlim@gmail.com>
2024-06-03 13:20:36 +1200

    20240603132036

--- src/components/ProjectItem.tsx
+++ src/components/ProjectItem.tsx
@@ -1,8 +1,11 @@
-import { useEffect } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import type Project from '../types/Project';
 import type CustomDocument from '../types/CustomDocument';
 import LazyLoad from "vanilla-lazyload";
 import { NavLink } from 'react-router-dom';
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+import { Zoom } from 'yet-another-react-lightbox/plugins';
 
 declare global {
 	interface Document extends CustomDocument { }
@@ -15,14 +18,37 @@ if (!document.lazyLoadInstance) {
 }
 
 const ProjectItem = ({ cssClasses, project }: { cssClasses: string, project: Project }) => {
+	const [isOpen, setOpen] = useState(false);
+	const zoomRef = useRef(null);
+
 	useEffect(() => {
 		document.lazyLoadInstance.update();
-	}, []);
+
+		if (isOpen) {
+			document.documentElement.style.setProperty('--scrollbar-track', '#000000');
+			document.documentElement.style.setProperty('--scrollbar-thumb', '#000000');
+			document.documentElement.style.setProperty('--scrollbar-thumb-hover', '#000000');
+
+		} else {
+			document.documentElement.style.setProperty('--scrollbar-track', '#384051');
+			document.documentElement.style.setProperty('--scrollbar-thumb', '#ffc91e');
+			document.documentElement.style.setProperty('--scrollbar-thumb-hover', '#ad8500');
+		}
+	}, [isOpen]);
 
 	return (
 		<div className={cssClasses + ` relative`} >
-			<div className={`h-[120dvw] xs:h-[65dvw] sm:h-[65dvw] md:h-[65dvw] lg:h-[50dvw] xl:h-[35dvw] lazy`} data-bg={'/images/' + project.image}>
-			</div>
+			<div onClick={() => setOpen(true)} className={`h-[120dvw] xs:h-[65dvw] sm:h-[65dvw] md:h-[65dvw] lg:h-[50dvw] xl:h-[35dvw] lazy`} data-bg={'/images/' + project.image}></div>
+
+			<Lightbox
+				open={isOpen}
+				close={() => setOpen(false)}
+				slides={[
+					{ src: '/images/' + project.image },
+				]}
+				plugins={[Zoom]}
+				zoom={{ ref: zoomRef }}
+			/>
 			<div className="lg:absolute lg:left-[2.5rem] lg:bottom-[2.5rem]">
 				<div className="font-primary font-normal text-[1.25rem] lg:text-[1.5625rem] text-white mt-[0.9375rem]">{project.name}</div>
 				<div className="hidden lg:block font-primary font-medium text-[1.125rem] text-white mb-[1.875rem]">https://google.com</div>
