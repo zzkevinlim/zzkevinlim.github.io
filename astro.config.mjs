07145ec3b503f663dd2919e5fc105f282bcffc99
Kevin Lim <zzkevinlim@gmail.com>
2025-04-27 13:36:27 +1200

    install tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
 	// Enable React to support React JSX components.
 	integrations: [react()],
+	vite: {
+		plugins: [tailwindcss()],
+	  },
 });
