c9b35464c18a3072491a540f170fe7e9e8514818
Kevin Lim <zzkevinlim@gmail.com>
2025-06-30 16:13:19 +1200

    20250630161319

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 function App() {
   return (
     <main className="container mx-auto px-4">
-      <h1 className="font-medium text-[100px]">Your Vision, My Mission</h1>
-      <h1 className="ffont-google-sans-display ont-medium text-[100px]">Your Vision, My Mission</h1>
+      <h1 className="font-google-sans-display font-medium text-[100px]">Your Vision, My Mission</h1>
+      <h1 className="font-google-sans-display font-medium text-[100px]">您的愿景，我的使命</h1>
       <p className="text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, eum rem? Exercitationem minus quisquam reprehenderit iste tenetur dolorum, labore explicabo.</p>
       <p className="font-google-sans-display text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, eum rem? Exercitationem minus quisquam reprehenderit iste tenetur dolorum, labore explicabo.</p>
     </main>
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+
+import { createBrowserRouter, RouterProvider } from "react-router";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={
+      createBrowserRouter([
+        {
+          path: '/',
+          element: <App />,
+        },
+      ])
+    } />
   </StrictMode>,
 )
