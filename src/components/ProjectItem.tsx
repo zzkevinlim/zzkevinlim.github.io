103a103956b4f1a6ba5df7d5ec7df340139bc9de
Kevin Lim <zzkevinlim@gmail.com>
2025-07-25 15:07:06 +1200

    20250725150706

--- src/components/ProjectItem.tsx
+++ src/components/ProjectItem.tsx
@@ -43,23 +43,23 @@ const ProjectItem = ({ cssClasses, project }: { cssClasses: string, project: Pro
 				<div className='hidden lg:block absolute inset-0 bg-gradient-to-r from-primary-500 to-[125%] opacity-0 group-hover/ancestor:opacity-100 transition-all duration-1000'></div>
 				<div className='hidden lg:block absolute left-[2.5rem] bottom-[2.5rem] overflow-hidden'>
 					<div className='hidden lg:block translate-y-[70%] group-hover/ancestor:translate-y-0 duration-300' style={{ transition: 'transform 0.6s cubic-bezier(0.68, -0.6, 0.32, 1.6)' }}>
-						<div className='font-primary font-medium text-[1.25rem] text-white mt-0 group-hover/ancestor:text-secondary-500 transition-all duration-300'>{project.name}</div>
+						<div className='font-primary font-medium text-[1.625rem] text-white mt-0 group-hover/ancestor:text-secondary-500 transition-all duration-300'>{project.name}</div>
 						<div className='font-primary font-medium text-[1.125rem] text-white mb-[1.25rem] group-hover/ancestor:text-secondary-500 transition-all duration-300'>{project.description}</div>
 						<div onClick={() => setOpen(true)} className='cursor-pointer'>
 							<div className='relative inline-block group mb-[0.3125rem]'>
 								<div className='absolute left-0 bottom-[-0.3125rem] w-[50%] h-[0.0625rem] bg-white group-hover:w-full transition-all duration-300'></div>
-								<span className='font-primary font-normal text-[0.9375rem] text-white tracking-widest uppercase'>Take a Look</span>
+								<span className='font-primary font-normal text-[0.9375rem] text-secondary-500 tracking-widest uppercase'>Take a Look</span>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
 			<div className='block lg:hidden mt-[0.9375rem]'>
-				<div className='font-primary font-medium text-[1.25rem] text-white mb-[0.625rem]'>{project.name}</div>
+				<div className='font-primary font-medium text-[1.625rem] text-white mb-[0.625rem]'>{project.name}</div>
 				<div onClick={() => setOpen(true)} className='cursor-pointer'>
 					<div className='relative inline-block group mb-[0.3125rem]'>
 						<div className='absolute left-0 bottom-[-0.3125rem] w-[50%] h-[0.0625rem] bg-white group-hover:w-full transition-all duration-300'></div>
-						<span className='font-primary font-normal text-[0.875rem] text-white tracking-widest uppercase'>Take a Look</span>
+						<span className='font-primary font-normal text-[0.875rem] text-secondary-500 tracking-widest uppercase'>Take a Look</span>
 					</div>
 				</div>
 			</div>
