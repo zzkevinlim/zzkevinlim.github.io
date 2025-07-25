cafa2b9a89aca137cc14b2ce5a89fc6e7675d542
Kevin Lim <zzkevinlim@gmail.com>
2025-07-25 15:29:01 +1200

    20250725152901

--- src/components/TagLine.tsx
+++ src/components/TagLine.tsx
@@ -1,8 +1,8 @@
 const TagLine = () => {
 	return (
 		<div className='ml-0 xs:ml-0 sm:ml-0 md:ml-[6dvw] lg:ml-[6dvw] xl:ml-[6.875rem] mt-[10rem] xs:mt-[10rem] sm:mt-[10.625rem] md:mt-[11.25rem] lg:mt-[15.625rem] xl:mt-[15.625rem] mb-[3.75rem] xs:mb-[3.75rem] sm:mb-[3.75rem] md:mb-[5.625rem] lg:mb-[5.625rem] xl:mb-[5.625rem]'>
-			<div className='font-primary font-normal text-[9dvw] xs:text-[9dvw] sm:text-[8dvw] md:text-[7dvw] lg:text-[6dvw] xl:text-[5.3125rem] leading-tight text-white'>Kevin Lim Development</div>
-			<div className='font-primary font-normal text-[1.625rem] leading-tight text-white'>Your vision, my mission &mdash; crafting digital experiences with purpose, personality and people in mind, <mark className="bg-primary-500">one pixel at a time</mark>.</div>
+			<div className='font-primary font-normal text-[9dvw] xs:text-[9dvw] sm:text-[8dvw] md:text-[7dvw] lg:text-[6dvw] xl:text-[5.3125rem] leading-tight text-white mb-[0.9375rem] lg:mb-0'>Kevin Lim Development</div>
+			<div className='font-primary font-normal text-[1.625rem] leading-tight text-white'><mark className="bg-primary-500">Your vision, my mission</mark> &mdash; crafting digital experiences with purpose, personality and people in mind, <mark className="bg-primary-500">one pixel at a time</mark>.</div>
 		</div>
 	);
 }
