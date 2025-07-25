a4ea9d3675c62bafe5a348501f7d406a3c2f9a69
Kevin Lim <zzkevinlim@gmail.com>
2025-07-25 15:25:58 +1200

    20250725152558

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const TagLine = () => {
 	return (
 		<div className='relative w-full'>
-			<div className='absolute left-1/2 bottom-0 -translate-x-1/2 font-primary text-[1rem] text-white tracking-wide text-center my-[0.9375rem]'>est. 2015&ndash;2025 Kevin Lim Development &bull; Malaysia &bull; New Zealand &bull; Australia</div>
+			<div className='absolute left-1/2 bottom-0 -translate-x-1/2 w-full font-primary text-[1rem] text-white tracking-wide text-center my-[0.9375rem]'>est. 2015&ndash;2025 Kevin Lim Development &bull; Malaysia &bull; New Zealand &bull; Australia</div>
 		</div>
 	);
 }
